b1a20b714a1539109fca6839f1815a658354a6b4
rafaelsilva2k <rafaelsilva2k256@gmail.com>
2026-05-27 15:38:49 -0600

    removed useefect

--- .history/src/App_20260527153652.js
+++ .history/src/App_20260527153652.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React from "react";
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Servicios from './components/Servicios';
